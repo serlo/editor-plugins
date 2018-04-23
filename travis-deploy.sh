ed0e75e89664919c462b674e69f4ee064c18b267
Jonas Keinholz <jonas@keinholz.com>
2018-04-23 11:57:45 +0200

    chore: initial deploy

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-
-set -euo pipefail
-
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
-lerna publish --skip-git --repo-version=$(git describe --tag) --yes --force-publish=*
+yarn lerna publish --skip-git --repo-version=$(git describe --tag) --yes --force-publish=*
