1382e2207c47f4e221c825cd900a4ee127f25478
Jonas Keinholz <jonas@keinholz.com>
2019-01-04 16:13:42 +0100

    chore(html-renderer): update to @splish-me/editor@0.3.0

--- packages/html-renderer/src/index.tsx
+++ packages/html-renderer/src/index.tsx
@@ -1,4 +1,4 @@
-import { createRenderer } from '@splish-me/editor-core/lib/renderer.component'
+import { createRenderer } from '@splish-me/editor-core-renderer'
 import * as React from 'react'
 
 const gridClass = (size = 12) => `col-sm-${size} col-xs-12`
@@ -7,16 +7,16 @@ export const HtmlRenderer = createRenderer({
   renderContainer({ children }) {
     return <div className="row">{children}</div>
   },
-  renderRow({ row, children }) {
+  renderRow({ children }) {
     return (
-      <div key={row.id} className="row">
+      <div className="row">
         {children}
       </div>
     )
   },
   renderCell({ cell, children }) {
     return (
-      <div key={cell.id} className={gridClass(cell.size)}>
+      <div className={gridClass(cell.size)}>
         {children}
       </div>
     )
