98062104215b53c4b1c43b1b962cf68d5f19a168
Jonas Keinholz <jonas@keinholz.com>
2019-01-04 16:28:24 +0100

    feat(editor-plugins-renderer): initial commit

---
+++ packages/editor-plugins-renderer/src/index.ts
@@ -0,0 +1,24 @@
+// import blockquote from '@serlo-org/editor-plugin-blockquote/lib/index.render'
+// import geogebra from '@serlo-org/editor-plugin-geogebra/lib/index.render'
+// import highlight from '@serlo-org/editor-plugin-highlight/lib/index.render'
+// import hint from '@serlo-org/editor-plugin-hint/lib/index.render'
+// import createImageRenderPlugin from '@serlo-org/editor-plugin-image/lib/index.render'
+// import injection from '@serlo-org/editor-plugin-injection/lib/index.render'
+// // TODO: create index.render
+// import license from '@serlo-org/editor-plugin-license'
+// import matchingExercise from '@serlo-org/editor-plugin-matching-exercise/lib/index.render'
+// import scMcExercise from '@serlo-org/editor-plugin-sc-mc-exercise/lib/index.render'
+// import solution from '@serlo-org/editor-plugin-solution/lib/index.render'
+// // TODO: create index.render
+// import spoiler from '@serlo-org/editor-plugin-spoiler'
+// import table from '@serlo-org/editor-plugin-table/lib/index.render'
+// import inputExercise from '@serlo-org/editor-plugin-input-exercise/lib/index.render'
+// import stepByStep from '@serlo-org/editor-plugin-step-by-step/lib/index.render'
+// import equations from '@serlo-org/editor-plugin-equations/lib/index.render'
+// import anchor from '@serlo-org/editor-plugin-anchor/lib/index.render'
+//
+// import pluginFactory, { Plugin, PluginRegistry } from './plugins'
+// import { slateRenderPlugin } from './slate.render'
+import { createPlugins } from '@serlo-org/editor-plugins-registry'
+
+export default createPlugins({})
