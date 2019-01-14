095018a987eec216a10c51a45472b22a4eb297b5
Jonas Keinholz <jonas@keinholz.com>
2019-01-14 15:39:29 +0100

    fix(editor-ui): fix styled export

--- packages/editor-ui/src/icon.tsx
+++ packages/editor-ui/src/icon.tsx
@@ -2,4 +2,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export const Icon = FontAwesomeIcon
 
-export * from '@fortawesome/free-solid-svg-icons'
+export {
+  faCheck,
+  faLink,
+  faImages,
+  faMinus,
+  faPlus,
+  faSpinner,
+  faTimes,
+  faTrashAlt
+} from '@fortawesome/free-solid-svg-icons'
