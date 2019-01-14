dbf0d47134c6ad4982aea5c7dfda1640b99c4631
Jonas Keinholz <jonas@keinholz.com>
2019-01-14 14:20:40 +0100

    chore: update dependencies

--- packages/demo/__stories__/demo/index.tsx
+++ packages/demo/__stories__/demo/index.tsx
@@ -1,8 +1,6 @@
 import { storiesOf } from '@storybook/react'
 import * as React from 'react'
-import { renderToString } from 'react-dom/server'
 
-import 'font-awesome/css/font-awesome.css'
 import 'katex/dist/katex.css'
 
 import { renderEditor, renderRenderer } from '@serlo/storybook-helpers'
