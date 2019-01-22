b7d9e566bbc3aadf3a32ef7551234a79830521db
Jonas Keinholz <jonas@keinholz.com>
2019-01-22 16:44:02 +0100

    chore: fix plop generator
    Close #55

--- plop-template/foobar-renderer/src/index.ts
+++ plop-template/foobar-renderer/src/index.ts
@@ -3,8 +3,6 @@ import { RendererPlugin } from '@splish-me/editor'
 import { {{properCase name}}Renderer } from './renderer'
 
 export const {{camelCase name}}RendererPlugin: RendererPlugin<{{properCase name}}PluginState > = {
-  name: '@serlo-org/{{dashCase name}}',
-  version: '0.0.0',
   Component: {{properCase name}}Renderer
 }
 
--- plop-template/foobar/src/index.ts
+++ plop-template/foobar/src/index.ts
@@ -1,14 +1,11 @@
 import {
-  {{properCase name}}PluginState,
-  {{properCase name}}Renderer
+  {{properCase name}}PluginState
 } from '@serlo/editor-plugin-{{dashCase name}}-renderer'
 import { Plugin } from '@splish-me/editor'
 
 import { {{properCase name}}Editor } from './editor'
 
 export const {{camelCase name}}Plugin: Plugin<{{properCase name}}PluginState> = {
-  name: '@serlo-org/{{dashCase name}}',
-  version: '0.0.0',
   Component: {{properCase name}}Editor,
   text: '{{name}}',
 
